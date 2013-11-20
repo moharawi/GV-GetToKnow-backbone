395041f12697cbb46c22ab17d9714ae906dd952b
Mohammad Hadhrawi <mkh@mit.edu>
2013-11-19 19:27:58 -0500

    Updated Homework

--- app/views.js
+++ app/views.js
@@ -1,9 +1,13 @@
 var splashView; // HACK: needs to be global so template can access country data
 
 (function($){
+var tst1 = null;
+var tst2 = null;
+var prev = false;
+var cArr = [];
 
 var SplashView = Backbone.View.extend({
-
+    
     events: {
       "change #gv-country":    "lookupCountry",
     },
@@ -11,8 +15,36 @@ var SplashView = Backbone.View.extend({
     initialize: function(){
         this.loadCountryData();
         this.render();
+        
+        this.routesHit = 0;
+        //keep count of number of routes handled by your application
+        Backbone.history.on('route', function() { this.routesHit++; }, this);
     },
      
+    back: function() {
+        if(this.routesHit > 1) {
+          //more than one route hit -> user did not land to current page directly
+          //window.history.back();
+          alert(">1");
+        } else {
+          //otherwise go to the home page. Use replaceState if available so
+          //the navigation doesn't create an extra history entry
+          //this.navigate('app/', {trigger:true, replace:true});
+          //Backbone.history.back();
+          //alert("else");
+          prev = true;
+          cArr.pop();
+          if(cArr.length > 0){
+            this.lookupCountry(cArr[cArr.length - 1]);
+            $('#gv-country').val(cArr[cArr.length - 1]);
+          }else{
+            prev = false;
+            this.render();
+          }
+        }
+        console.log(this.routesHit);
+    },
+
     // render the whole app
     render: function(){
         $(this.el).load("templates/app.template");
@@ -27,8 +59,17 @@ var SplashView = Backbone.View.extend({
     },
     
     // if the country is a valid one, update the map and content
-    lookupCountry: function(){
-        var currentCountry = $('#gv-country').val();
+    lookupCountry: function(tst){
+        console.log("lookupCountr  ->  "+cArr)
+        if(prev){
+            //tst1 = tst;console.log(tst);
+            var currentCountry = tst;
+        }else{
+            //tst1 = tst;console.log(tst);
+            var currentCountry = $('#gv-country').val();
+        }
+        
+        //var currentCountry = $('#gv-country').val();
         if (_.has(this.countryToPath,currentCountry) ){
             rssUrl = "http://globalvoicesonline.org"+this.countryToPath[currentCountry]+"feed";
             $('#gv-country').attr('disabled', 'disabled');
@@ -36,6 +77,15 @@ var SplashView = Backbone.View.extend({
             $('#gv-loading').show();
             this.updateGlobalVoicesContent(rssUrl);
             this.updateBackgroundMap(currentCountry);
+            
+            
+            
+            //tst2 = tst1;
+            if(!prev){
+                cArr.push(currentCountry);
+            }
+            prev = false;
+            
         }
     },
 
@@ -46,11 +96,14 @@ var SplashView = Backbone.View.extend({
             url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(rssUrl),
             dataType: 'json',
             success: function(data) {
-                stories = _.first(data.responseData.feed.entries,3);
+                stories = _.first(data.responseData.feed.entries,6);
                 $('#gv-country').attr('disabled', false);
                 that.updateOneStory(stories[0], '#gv-story-1');
                 that.updateOneStory(stories[1], '#gv-story-2');
                 that.updateOneStory(stories[2], '#gv-story-3');
+                that.updateOneStory(stories[2], '#gv-story-4');
+                that.updateOneStory(stories[2], '#gv-story-5');
+                that.updateOneStory(stories[2], '#gv-story-6');
                 $('#gv-loading').hide();
                 $('.gv-story').show();
             }
@@ -89,9 +142,29 @@ var SplashView = Backbone.View.extend({
 
 });
 
+    
+    AppRouter = Backbone.Router.extend({
+        routes: {
+            "*actions": "defaultRoute" // matches http://example.com/#anything-here
+        }
+    });
+    // Initiate the router
+    var app_router = new AppRouter;
+
+    app_router.on('route:defaultRoute', function(actions) {
+        //alert(actions);
+    })
+
+    // Start Backbone history a necessary step for bookmarkable URL's
+    Backbone.history.start();
+    
+
+
+
 splashView = new SplashView;
 
 // kick off the app!
 $("#app").html(splashView.el);
 
 })(jQuery);
+var AppRouter;
